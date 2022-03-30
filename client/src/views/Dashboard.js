d464a74b47106bdd6f17718142767b87644c44f3
Lisa <lb.broadhead@gmail.com>
2022-03-30 16:44:48 -0500

    first commit

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Spouse from '../components/Spouse';
 import UserContext from '../context/UserContext';
+import styled from '../css/ViewsCSS/Dashboard.modules.css';
 
 const Dashboard = (props) => {
     const { user, setUser, userIDs, spouseIDs } = useContext(UserContext)
 
     return(
-        <div>
+        <div className={styled.container}>
             <h1>Welcome Back <span> {user.firstName} </span>!</h1> 
             <h3>May you never loose track of your siblings again!</h3>
             <div>
