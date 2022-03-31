e82072f64a36bde080ed2de27dfcb3827b44adb0
Lisa <lb.broadhead@gmail.com>
2022-03-30 21:46:23 -0500

    first commit

--- client/src/components/UserBlock.js
+++ client/src/components/UserBlock.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import UserContext from '../context/UserContext';
 import Sibling from './Sibling';
 import empty from '../img/empty.jpg';
@@ -6,16 +6,24 @@ import styled from '../css/ComponentsCSS/UserBlock.module.css';
 
 const UserBlock = (props) => {
     const { user, setUser, userIDs } = useContext(UserContext)
+    const [birthday,setBirthday] = useState()
     //1. block yourself - check
     //2. block people you're already connected with
     //3. resrict to one spouse, 2 parents, ect?
+
+    
+    // useEffect(() => {
+
+
+    // }, []);
 
     return(
         <>
         {
             userIDs === props.user._id ? "" : <div  className={styled.UserBlock}><div className={styled.imgWrp}>
                 { props.user.image === "empty.jpg" ?  <img src={empty} alt={props.user.firstName} /> : <img src={props.user.image} alt={props.user.firstName} />}</div>
-                <p className={styled.name}>{props.user.firstName} {props.user.lastName}</p><Sibling id={props.user._id} /></div>
+                <p className={styled.name}>{props.user.firstName} {props.user.lastName}</p><p className={styled.birthday}> {props.user.birthday}</p><Sibling id={props.user._id} />
+                </div>
         }
         </>
         
